c7c0b3a7b83b49c63f44d19c5ee33c063f02ff6e
sonupooja <sonupooja2398@gmail.com>
2021-10-29 12:01:58 +0530

    Added:delete contact

--- src/com/bridglab/Stream/AddressBookSystem.java
+++ src/com/bridglab/Stream/AddressBookSystem.java
@@ -1,9 +1,8 @@
 package com.bridglab.Stream;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.Scanner;
 public class AddressBookSystem {
-
 
     ArrayList<Contacts> arrayDetails = new ArrayList<Contacts>();
     static Scanner sc = new Scanner(System.in);
@@ -34,7 +33,7 @@ public class AddressBookSystem {
     }
 
     /**
-     *This method is used to edit the deatils in address book
+     *This method is used to edit the details in address book
      */
 
     public void editDetails() {
@@ -86,8 +85,26 @@ public class AddressBookSystem {
             } else
                 System.out.println("Enter a valid First name");
         }
-
     }
+
+    /**
+     * This method is used to delete the contact details
+     */
+    public void deleteDetails() {
+        System.out.println("Confirm the first name of the person to delete contact");
+        String confirmName = sc.next();
+        for (int i = 0; i < arrayDetails.size(); i++) {
+
+            if (arrayDetails.get(i).getFirstName().equals(confirmName)) {
+                arrayDetails.remove(i);
+                System.out.println("List After removing"+arrayDetails);
+
+            } else {
+                System.out.println("Enter valid first name");
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         AddressBookSystem details = new AddressBookSystem();
@@ -96,7 +113,7 @@ public class AddressBookSystem {
         while (i == 0) {
             System.out.println("Welcome to Address Book Program");
             System.out.println("What do you want to do: ");
-            System.out.println("1.Add details.\n2.Edit details.");
+            System.out.println("1.Add details.\n2.Edit details.\n3.Delete Details.");
             int choose = sc.nextInt();
             switch (choose) {
                 case 1:
@@ -105,12 +122,14 @@ public class AddressBookSystem {
                 case 2:
                     details.editDetails();
                     break;
+                case 3:
+                    details.deleteDetails();
+                    break;
                 default:
                     i = 1;
                     System.out.println("Wrong option");
                     break;
             }
         }
-
     }
 }
